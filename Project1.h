9c51bed64fe2957be439d828dd21232c8e306d21
Anymous <96421607+Hurmatillo@users.noreply.github.com>
2022-01-08 16:08:39 +0500

    Add files via upload

---
+++ Project1.h
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------
+
+#ifndef Project1H
+#define Project1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *Image1;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
---
+++ Unit7.dfm
@@ -0,0 +1,21 @@
+object Form7: TForm7
+  Left = 0
+  Top = 0
+  Caption = 'Form7'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  PixelsPerInch = 96
+  TextHeight = 15
+  object Image1: TImage
+    Left = 24
+    Top = 32
+    Width = 105
+    Height = 105
+  end
+end
